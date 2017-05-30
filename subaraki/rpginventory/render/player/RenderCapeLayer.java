ca4887acde1e501c35286a0dfce5ac6890896fdb
ArtixAllMighty <robbeenaxel@hotmail.com>
2017-05-30 14:24:19 +0200

    changed to custom cape model for better texture mapping.

--- subaraki/rpginventory/render/player/RenderCapeLayer.java
+++ subaraki/rpginventory/render/player/RenderCapeLayer.java
@@ -1,34 +1,38 @@
 package subaraki.rpginventory.render.player;
 
+import lib.modelloader.ModelHandle;
 import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderPlayer;
 import net.minecraft.client.renderer.entity.layers.LayerRenderer;
+import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.init.Items;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.MathHelper;
 import subaraki.rpginventory.capability.playerinventory.RpgInventoryData;
 import subaraki.rpginventory.item.RpgInventoryItem;
+import subaraki.rpginventory.mod.RpgInventory;
 
 public class RenderCapeLayer implements LayerRenderer<AbstractClientPlayer>{
 
-    private final RenderPlayer playerRenderer;
+	private final RenderPlayer playerRenderer;
+	private ModelHandle cape = ModelHandle.of(RpgInventory.MODID+":render/cape");
 
-    public RenderCapeLayer(RenderPlayer playerRendererIn)
-    {
-        this.playerRenderer = playerRendererIn;
-    }
+	public RenderCapeLayer(RenderPlayer playerRendererIn)
+	{
+		this.playerRenderer = playerRendererIn;
+	}
 
-    @Override
-    public void doRenderLayer(AbstractClientPlayer player, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale)
-    {
-    	/**copy of @Link LayerCape*/
-    	RpgInventoryData inventory = RpgInventoryData.get(player);
-    	ItemStack cloak = inventory.getCloak();
-    	
-    	
-    	if (cloak == ItemStack.EMPTY)
+	@Override
+	public void doRenderLayer(AbstractClientPlayer player, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale)
+	{
+		/**copy of @Link LayerCape*/
+		RpgInventoryData inventory = RpgInventoryData.get(player);
+		ItemStack cloak = inventory.getCloak();
+
+
+		if (cloak == ItemStack.EMPTY)
 			return;
 
 		if (cloak.getItem() == null)
@@ -40,9 +44,21 @@ public class RenderCapeLayer implements LayerRenderer<AbstractClientPlayer>{
 		if (itemstack == ItemStack.EMPTY || itemstack.getItem() != Items.ELYTRA){
 
 			GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-			playerRenderer.bindTexture(item.getRenderOnPlayerTexture());
+
+			String cape_texture = item.getRenderOnPlayerTexture().getResourcePath();
+
+			if (!cape.getTextureReplacements().containsKey("0") || cape.getTextureReplacements().containsKey("0") && !cape.getTextureReplacements().get("0").equals(cape_texture))
+				cape = cape.replace("0", cape_texture );
+
+			playerRenderer.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
 			GlStateManager.pushMatrix();
-			GlStateManager.translate(0.0F, 0.0F, 0.125F);
+
+			float offsetX = 0.0625F*5F;
+			float offsetZ = 0.05f;
+
+			GlStateManager.translate(offsetX, 0.0F, offsetZ);//rotate to center
+
+			GlStateManager.translate(-offsetX, 0.0F, 0.1f);
 			double d0 = player.prevChasingPosX + (player.chasingPosX - player.prevChasingPosX) * (double)partialTicks - (player.prevPosX + (player.posX - player.prevPosX) * (double)partialTicks);
 			double d1 = player.prevChasingPosY + (player.chasingPosY - player.prevChasingPosY) * (double)partialTicks - (player.prevPosY + (player.posY - player.prevPosY) * (double)partialTicks);
 			double d2 = player.prevChasingPosZ + (player.chasingPosZ - player.prevChasingPosZ) * (double)partialTicks - (player.prevPosZ + (player.posZ - player.prevPosZ) * (double)partialTicks);
@@ -58,7 +74,7 @@ public class RenderCapeLayer implements LayerRenderer<AbstractClientPlayer>{
 			{
 				yRotation = 0.0F;
 			}
-			
+
 			//added a check so the cape doesnt fly over your head when going to fast !
 			if (yRotation > 178.0F)
 			{
@@ -78,15 +94,20 @@ public class RenderCapeLayer implements LayerRenderer<AbstractClientPlayer>{
 			GlStateManager.rotate(-zRotation / 2.0F, 0.0F, 1.0F, 0.0F);
 			GlStateManager.rotate(180.0F, 0.0F, 1.0F, 0.0F);
 			if (player.isSneaking())//this isn't necessarily needed, but the space between cape and player while sneaking was killing me !
-	            GlStateManager.translate(0.0F, 0.0F, 0.1F);
-			playerRenderer.getMainModel().renderCape(0.0625F);
+				GlStateManager.translate(0.0F, 0.1F, 0.05F);
+
+			//shift back to center
+			GlStateManager.translate(-offsetX, 0.0F, -offsetZ);
+
+			cape.render();
+
 			GlStateManager.popMatrix();
 		}
-    }
+	}
 
-    @Override
-    public boolean shouldCombineTextures()
-    {
-        return false;
-    }
+	@Override
+	public boolean shouldCombineTextures()
+	{
+		return false;
+	}
 }
