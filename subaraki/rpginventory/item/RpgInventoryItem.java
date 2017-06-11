9a90ec7e6eae919102654f23e1527ff87d5899cf
ArtixAllMighty <robbeenaxel@hotmail.com>
2017-06-11 14:21:03 +0200

    managed imports

--- subaraki/rpginventory/item/RpgInventoryItem.java
+++ subaraki/rpginventory/item/RpgInventoryItem.java
@@ -5,7 +5,6 @@ import java.util.List;
 import net.minecraft.entity.EntityTracker;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.item.EnumAction;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
